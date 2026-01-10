7f8c0df536bdfd38f10245262daf8a808e0027e2
henok3878 <81500872+henok3878@users.noreply.github.com>
2026-01-10 04:50:03 -0500

    feat(model): implement rmsnorm

---
+++ src/model/rmsnorm.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(d_model))
+
+    def forward(self, x):
+        output_dtype = x.dtype
+        # force float32 for stability
+        x = x.to(torch.float32)
+        variance = x.pow(2).mean(-1, keepdim=True)
+        x = x * torch.rsqrt(variance + self.eps)
+        return self.weight * x.to(output_dtype)
